c0e94ebd463f8e1874e362c7541437d1052d1dad
enzoblain <blenzodu57@gmail.com>
2025-05-08 19:34:47 +0300

    Add support for all available timeranges

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod server;
 pub mod structures;
 
 pub use structures::Candle;
+pub use structures::TIMERANGES;
 
 pub use server::DB_CLIENT;
 pub use server::WEBSOCKET_URL;
--- src/structures.rs
+++ src/structures.rs
@@ -1,4 +1,7 @@
+use lazy_static::lazy_static;
+use std::sync::Arc;
 use serde::Serialize;
+use tokio::sync::Mutex;
 
 // General structure for a candle
 #[derive(Clone, Debug, Default, Serialize)]
@@ -8,9 +11,23 @@ pub struct Candle {
     pub symbol: String,
     pub timerange: String,
     pub open: f64,
-    pub close: f64,
+    pub close: Option<f64>,
     pub high: f64,
     pub low: f64,
     pub price: Option<f64>,
     pub volume: f64,
+    pub usdt_volume: f64,
+}
+
+// Store all the different timeranges we can use
+lazy_static! {
+    pub static ref TIMERANGES: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(vec![
+        "1m".to_string(),
+        "5m".to_string(),
+        "15m".to_string(),
+        "30m".to_string(),
+        "1h".to_string(),
+        "4h".to_string(),
+        "1d".to_string(),
+    ]));
 }
